d7c3b9817fb19217471213f91881e2aa6ca93566
Ertan Donmez <ertandonmez1@gmail.com>
2024-08-26 13:21:50 +0300

    first commit

---
+++ JavaScript/calculateSum.js
@@ -0,0 +1,19 @@
+1. Write a JavaScript function to calculate the sum of two numbers.
+
+function calculateSum () {
+    let a = 4
+    let b = 20
+
+    sumOfTwo = parseInt(a) + parseInt(b)
+    console.log(sumOfTwo)
+}
+
+calculateSum ()
+
+// Or:
+// function calculateSum(a, b) {
+//     return parseInt(a) + parseInt(b);
+// }
+
+// let result = calculateSum(4, 20);
+// console.log(result);
